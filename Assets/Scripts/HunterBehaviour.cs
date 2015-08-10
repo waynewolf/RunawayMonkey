dfaed38fac2518f83c3d2a25035c0b7fc05c08bf
Wayne Wolf <wayne.ron.wu@gmail.com>
2015-08-10 22:05:29 +0800

    hunter hit by bannana effect implemented
    Signed-off-by: Wayne Wolf <wayne.ron.wu@gmail.com>

--- Assets/Scripts/HunterBehaviour.cs
+++ Assets/Scripts/HunterBehaviour.cs
@@ -4,6 +4,12 @@ using System.Collections;
 public class HunterBehaviour : MonoBehaviour {
 	public GameObject attackedEffect;
 
+	private Animator _animator;
+
+	void Start() {
+		_animator = GetComponent<Animator>();
+	}
+
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.tag == "Banana") {
 			Destroy(other.gameObject);
@@ -11,7 +17,12 @@ public class HunterBehaviour : MonoBehaviour {
 			spawnPos.y += 1f;
 			spawnPos.z = 0;
 			Instantiate(attackedEffect, spawnPos, Quaternion.identity);
+			_animator.SetBool("Hit", true);
 		}
 	}
-	
+
+	// called by animation event
+	void ExitHitByBanana() {
+		_animator.SetBool("Hit", false);
+	}
 }
