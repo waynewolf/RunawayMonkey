ff22df26ef0460a667dfdcaf5dee03cc833ca198
Wayne Wolf <wayne.ron.wu@gmail.com>
2015-08-14 18:43:49 +0800

    add block trigger at head of middle platform dynamically
    Signed-off-by: Wayne Wolf <wayne.ron.wu@gmail.com>

---
+++ Assets/Scripts/AddBlockTriggerAtHead.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (BoxCollider2D))]
+public class AddBlockTriggerAtHead : MonoBehaviour {
+	
+	void Start () {
+		BoxCollider2D boxColliderAttached = GetComponent<BoxCollider2D>();
+		if (!boxColliderAttached) {
+			Debug.LogError("The game object " + gameObject.name +
+			               " must attach a boxcollider that is the same width of the sprite");
+			return;
+		}
+		BoxCollider2D boxCollider = gameObject.AddComponent<BoxCollider2D>();
+		boxCollider.isTrigger = true;
+		// programablly add trigger, so the size is not affected by the
+		// scale of the attached game object, otherwise, monkey may be
+		// blocked far away which causes confusion. This is the reason
+		// why we don't use a child gameobject like 'block check' thing.
+		boxCollider.size = new Vector2(0.03f, boxColliderAttached.size.y);
+		boxCollider.offset = new Vector2(-0.5f * boxColliderAttached.size.x + 0.01f, 0f);
+	}
+
+}
