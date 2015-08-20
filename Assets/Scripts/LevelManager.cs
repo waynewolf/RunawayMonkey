ac00c8dc6419a6c28c271c8be107e6f3befdae69
Wayne Wolf <wayne.ron.wu@gmail.com>
2015-08-20 20:00:53 +0800

    remove inconsistent distance to hunter variable
    Signed-off-by: Wayne Wolf <wayne.ron.wu@gmail.com>

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -19,7 +19,7 @@ public class LevelManager : MonoBehaviour {
 	[HideInInspector]
 	public HunterBehaviour Hunter { get; private set; }
 	
-	private const float DISTANCE_TO_HUNTER = -7.5f;
+	private const float OFFSET_TO_HUNTER = -7.5f;
 	private const float INIT_HUNTER_Y_OFFSET = -0.7f;
 	private const float HUNTER_HIT_BACKWARD_DISTANCE = 1.5f;
 	private const float MAX_TIMEOUT_WHEN_HUNTER_OUT_OF_SCREEN = 2f;
@@ -28,7 +28,6 @@ public class LevelManager : MonoBehaviour {
 	private float _halfScreenWidthInUnit;
 	private float _currentSpeed;
 	private int _bananaNumber = 0;
-	private float _currentDistanceToHunter;
 	private float _elapsedTimeWhenHunterOutOfScreen;
 	private GameObject _shadow;
 	private Vector3 _shadowScale = Vector3.one;
@@ -37,12 +36,11 @@ public class LevelManager : MonoBehaviour {
 	void Awake() {
 		Instance = this;
 		Player = Instantiate(playerPrefab, Vector3.zero, Quaternion.identity) as MonkeyBehaviour;
-		Hunter = Instantiate(hunterPrefab, new Vector3(DISTANCE_TO_HUNTER,
+		Hunter = Instantiate(hunterPrefab, new Vector3(OFFSET_TO_HUNTER,
 					INIT_HUNTER_Y_OFFSET, 0), Quaternion.identity) as HunterBehaviour;
 		_shadow = Instantiate(shadowPrefab, new Vector3(0, -3f, 5f), Quaternion.identity) as GameObject;
 		GameManager.Instance.Player = Player;
 		_currentSpeed = GameManager.Instance.NormalSpeed;
-		_currentDistanceToHunter = -DISTANCE_TO_HUNTER;
 		_elapsedTimeWhenHunterOutOfScreen = 0f;
 		_halfScreenWidthInUnit = 1.0f * Screen.width / Screen.height * Camera.main.orthographicSize;
 		_bgStartPos = background.transform.position;
@@ -76,19 +74,17 @@ public class LevelManager : MonoBehaviour {
 			background.position = _bgStartPos + Vector3.left * newPosition;
 		} else if (Hunter.IsCatching()) {
 			// The monkey is blocked, hunter will catch the monkey in catchMonkeyTime seconds
-			float advanceDistance = Mathf.Abs (DISTANCE_TO_HUNTER) * Time.deltaTime / catchMonkeyTime;
-			_currentDistanceToHunter -= advanceDistance;
+			float advanceDistance = Mathf.Abs (OFFSET_TO_HUNTER) * Time.deltaTime / catchMonkeyTime;
 			Hunter.Forward(advanceDistance);
 		}
 
-		if (_currentDistanceToHunter > _halfScreenWidthInUnit) {
+		if (MonkeyHunterDistance() > _halfScreenWidthInUnit) {
 			_elapsedTimeWhenHunterOutOfScreen += Time.deltaTime;
 		}
 
 		if (_elapsedTimeWhenHunterOutOfScreen > MAX_TIMEOUT_WHEN_HUNTER_OUT_OF_SCREEN) {
 			_elapsedTimeWhenHunterOutOfScreen = 0f;
-			_currentDistanceToHunter = -DISTANCE_TO_HUNTER;
-			Hunter.MoveToX(DISTANCE_TO_HUNTER);
+			Hunter.MoveToX(OFFSET_TO_HUNTER);
 		}
 
 		if (_shadow.activeInHierarchy) {
@@ -96,6 +92,10 @@ public class LevelManager : MonoBehaviour {
 			_shadowScale.y = 1f - (Player.transform.position.y - (-3f)) / 10f;
 			_shadow.transform.localScale = _shadowScale;
 		}
+	}
+
+	private float MonkeyHunterDistance() {
+		return Player.transform.position.x - Hunter.transform.position.x;
 	}
 
 	public void StopMoving() {
@@ -143,8 +143,7 @@ public class LevelManager : MonoBehaviour {
 	}
 
 	public void HunterHit() {
-		if (_currentDistanceToHunter < 1.1f * _halfScreenWidthInUnit) {
-			_currentDistanceToHunter += HUNTER_HIT_BACKWARD_DISTANCE;
+		if (MonkeyHunterDistance() < 1.1f * _halfScreenWidthInUnit) {
 			Hunter.Backward(HUNTER_HIT_BACKWARD_DISTANCE);
 		}
 	}
@@ -159,9 +158,8 @@ public class LevelManager : MonoBehaviour {
 		GUIManager.Instance.EnableButtons();
 		Player.transform.position = Vector3.zero;
 		Player.Revived();
-		Hunter.MoveToX(DISTANCE_TO_HUNTER);
+		Hunter.MoveToX(OFFSET_TO_HUNTER);
 		Hunter.MonkeyRunaway();
-		_currentDistanceToHunter = -DISTANCE_TO_HUNTER;
 	}
 	
 }
