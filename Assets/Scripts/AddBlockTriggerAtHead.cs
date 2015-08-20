3a7930dc9a8cabe569a267f7a81bd5992ae986a6
Wayne Wolf <wayne.ron.wu@gmail.com>
2015-08-20 19:22:59 +0800

    the trigger at head of middle platform not created in some circumstances
    Signed-off-by: Wayne Wolf <wayne.ron.wu@gmail.com>

--- Assets/Scripts/AddBlockTriggerAtHead.cs
+++ Assets/Scripts/AddBlockTriggerAtHead.cs
@@ -13,11 +13,12 @@ public class AddBlockTriggerAtHead : MonoBehaviour {
 		}
 		BoxCollider2D boxCollider = gameObject.AddComponent<BoxCollider2D>();
 		boxCollider.isTrigger = true;
-		// programablly add trigger, so the size is not affected by the
+		// programmablly add trigger, so the size is not affected by the
 		// scale of the attached game object, otherwise, monkey may be
 		// blocked far away which causes confusion. This is the reason
 		// why we don't use a child gameobject like 'block check' thing.
-		boxCollider.size = new Vector2(0.03f, boxColliderAttached.size.y - 0.02f);
+		float sizeY = Mathf.Max(boxColliderAttached.size.y - 0.02f, 0.1f);
+		boxCollider.size = new Vector2(0.06f, sizeY);
 		boxCollider.offset = new Vector2(-0.5f * boxColliderAttached.size.x + 0.01f, 0f);
 	}
 
