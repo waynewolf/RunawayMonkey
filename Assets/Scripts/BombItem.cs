d14025b605aaf4e5855d35e75abd3a17d05cfe19
Wayne Wolf <wayne.ron.wu@gmail.com>
2015-08-26 23:53:01 +0800

    bomb item implemented
    Signed-off-by: Wayne Wolf <wayne.ron.wu@gmail.com>

---
+++ Assets/Scripts/BombItem.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombItem : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.tag == "Player") {
+			gameObject.SetActive(false);
+			// TODO: explosion animation
+			LevelManager.Instance.OnMonkeyGoingToDie();
+		}
+	}
+
+}
