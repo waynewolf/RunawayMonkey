3ec716603edbf13ea784102371c4824d88f33665
Wayne Wolf <wayne.ron.wu@gmail.com>
2015-08-27 23:52:38 +0800

    item collected sound effect added
    Signed-off-by: Wayne Wolf <wayne.ron.wu@gmail.com>

--- Assets/Scripts/StrawberryItem.cs
+++ Assets/Scripts/StrawberryItem.cs
@@ -3,11 +3,13 @@ using System.Collections;
 
 public class StrawberryItem : MonoBehaviour {
 	public GameObject _starEffectPrefab;
+	public AudioClip _itemCollectedSoundEffect;
 
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.tag == "Player") {
 			LevelManager.Instance.EatStrawberry(transform);
 			Instantiate(_starEffectPrefab, transform.position, Quaternion.identity);
+			SoundManager.Instance.PlaySound(_itemCollectedSoundEffect, transform.position);
 		}
 	}
 }
